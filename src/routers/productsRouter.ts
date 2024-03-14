8b7e36b07ffd709ab5e7e48290fef30e9af6985a
Woongsik <airwoong83@gmail.com>
2024-03-14 13:34:19 +0200

    Init

--- src/routers/productsRouter.ts
+++ src/routers/productsRouter.ts
@@ -1,55 +1,73 @@
-// product router
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
 
 type Product = {
   id: string;
-  name: string;
+  title: string;
   price: number;
+  description: string;
+  images: string[];
+  stockQuantity: number;
+  categoryId: number;
 };
 
-// fake database
 let products: Product[] = [
-  { id: "1", name: "product1", price: 1 },
-  { id: "2", name: "product2", price: 2 },
-  { id: "3", name: "product3", price: 3 },
+  {
+    id: '1',
+    title: 'Product 1',
+    price: 1,
+    description: 'Description 1',
+    images: ['product1 image'],
+    stockQuantity: 100,
+    categoryId: 1,
+  },
+  {
+    id: '2',
+    title: 'Product 2',
+    price: 2,
+    description: 'Description 2',
+    images: ['product2 image'],
+    stockQuantity: 30,
+    categoryId: 2,
+  },
+  {
+    id: '3',
+    title: 'Product 3',
+    price: 3,
+    description: 'Description 3',
+    images: ['product3 image'],
+    stockQuantity: 10,
+    categoryId: 3,
+  },
 ];
 
 const router = express.Router();
 
-// query
-// http://localhost:8080/api/v1/products?name=Generic
-// ?offset=0&limit=10
-// search
-// filter
+// method, endpoint, data 
 router.get("/", (request: Request, response: Response) => {
-  // query
-  const nameQuery = request.query.name as string;
-  console.log(request.query, "query");
-  const priceQuery = request.query.price as string;
-
-  products = products.filter((product) =>
-    product.name.toLowerCase().includes(nameQuery.toLowerCase())
-  );
-  // get product with less than priceQuery
-  response.status(200).json(products);
+  let matchedProducts: Product[] = products;
+  // Query
+  const nameQuery = request.query.title as string;
+  if (nameQuery) {
+    matchedProducts = matchedProducts.filter((product: Product) => 
+      product.title.toLowerCase().includes(nameQuery.toLowerCase()));
+  }
+  
+  response.status(200).json(matchedProducts);
 });
-
-// base url:"http://localhost:8080/api/v1/products/"
-// router.get("/", (request: Request, response: Response) => {
-//   response.status(200).json(products);
-// });
 
 router.post("/", (request: Request, response: Response) => {
   const newProduct = request.body;
   products.push(newProduct);
-  response.status(201).json(products);
+
+  response.status(200).json(products);
 });
 
-// base url:"http://localhost:8080/api/v1/products/:productId"
 router.delete("/:productId", (request: Request, response: Response) => {
   const productId = request.params.productId;
-  products = products.filter((item) => item.id !== productId);
-  response.sendStatus(204);
+  products = products.filter((product: Product) => { product.id !== productId});
+  
+  response.sendStatus(204); // with no content, use sendStatus
+  // response.status(200).json({ message: "delete item successfully" }); // Or send some message 
 });
 
 export default router;
--- src/routers/usersRouter.ts
+++ src/routers/usersRouter.ts
@@ -1,1 +1,54 @@
-//
+import express, { Request, Response } from 'express';
+
+type User = {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  role: 'customer' | 'admin';
+  avatar: string;
+  address: string;
+};
+
+let users: User[] = [
+  {
+    id: '1',
+    name: 'Ganesh',
+    email: 'user1@mail.com',
+    password: 'user1pasword',
+    role: 'customer',
+    avatar: 'user1Avatar',
+    address: 'user1Address',
+  },
+  {
+    id: '2',
+    name: 'Roshan',
+    email: 'user2@mail.com',
+    password: 'user2pasword',
+    role: 'customer',
+    avatar: 'user2Avatar',
+    address: 'user2Address',
+  },
+  {
+    id: '3',
+    name: 'Woong',
+    email: 'user3@mail.com',
+    password: 'user3pasword',
+    role: 'customer',
+    avatar: 'user3Avatar',
+    address: 'user3Address',
+  },
+];
+
+const router = express.Router();
+
+// method, endpoint, data 
+router.get("/", (request: Request, response: Response) => {
+  
+});
+
+router.post("/", (request: Request, response: Response) => {
+  
+});
+
+export default router;
