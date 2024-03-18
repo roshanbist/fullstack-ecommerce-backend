9cdc110b7e8561a17d4d325e083eab5b6f679de0
Woongsik <airwoong83@gmail.com>
2024-03-18 14:41:56 +0200

    Connect to mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,19 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import dotenv from 'dotenv';
 
 import productsRouter from './routers/productsRouter';
 import usersRouter from './routers/usersRouter';
 import categoriesRouter from './routers/categoriesRouter';
 import adminRouter from './routers/adminRouter';
 
-const PORT = 8080;
 const app = express(); // Create a server with express
 app.use(express.json()); // Return as json
 
-app.get('/', (request: Request, response: Response) => {
-  response.status(200).json('Hello world');
-});
+dotenv.config({ path: '.env' });
 
 app.use('/api/v1/products', productsRouter);
 app.use('/api/v1/users', usersRouter);
 app.use('/api/v1/categories', categoriesRouter);
 app.use('/api/v1/admin', adminRouter);
 
-app.listen(PORT, () => {
-  console.log(`server running at... http://localhost:${PORT}`);
-});
+export default app;
