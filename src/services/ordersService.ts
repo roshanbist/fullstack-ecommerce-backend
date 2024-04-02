b4325798bc53a11449cf0e07cada48464fec2309
Woongsik <airwoong83@gmail.com>
2024-04-02 09:39:49 +0300

    Add orderItems.product.category populate

--- src/services/ordersService.ts
+++ src/services/ordersService.ts
@@ -5,13 +5,14 @@ const getAllOrders = async (userId: string): Promise<OrderDocument[]> => {
     user: userId
   }).populate([
     { path: 'user', select: { userName: 1, address: 1 }},
-  { path: 'items.product.category'}  ]);
+    { path: 'items.product.category'}  ]);
 }
 
 const getOrderyById = async (orderId: string): Promise<OrderDocument | null> => {
   return await OrderModel.findById(orderId)
   .populate([
-    { path: 'user', select: { userName: 1, address: 1 }}
+    { path: 'user', select: { userName: 1, address: 1 }},
+    { path: 'items.product.category'} 
   ]);
 }
 
